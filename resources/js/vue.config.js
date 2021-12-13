3080eaae383ae4da001ddbce193a954c631c4b6f
WyomingCPA@yandex.ru <WyomingCPA@yandex.ru>
2021-12-14 02:11:20 +0300

    add dist

--- resources/js/vue.config.js
+++ resources/js/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = { 
-  publicPath: process.env.NODE_ENV === 'production' ? '/demo/corona-vue-free/preview/demo_1/' : '/' 
+  publicPath: process.env.NODE_ENV === 'production' ? '' : '' 
 }
