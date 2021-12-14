8808d5408c540acd3bd03e27f446ec0e26330225
WyomingCPA@yandex.ru <WyomingCPA@yandex.ru>
2021-12-14 14:00:53 +0300

    fix bug

--- resources/js/vue.config.js
+++ resources/js/vue.config.js
@@ -1,3 +1,11 @@
-module.exports = { 
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/' 
+module.exports = {
+  publicPath: '',
+  lintOnSave: false,
+  runtimeCompiler: true,
+  configureWebpack: {
+    //Necessary to run npm link https://webpack.js.org/configuration/resolve/#resolve-symlinks
+    resolve: {
+      symlinks: true,
+    }
+  }
 }
